c7fd3a7f49baee6772a01133c39c7a3a1e5f2026
정승환 <seunghw2@naver.com>
2024-04-23 16:39:37 +0900

    [fix] BaseEntity를 위한 어노테이션 추가

--- backend/befresh/src/main/java/com/a307/befresh/BefreshApplication.java
+++ backend/befresh/src/main/java/com/a307/befresh/BefreshApplication.java
@@ -2,12 +2,17 @@ package com.a307.befresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BefreshApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BefreshApplication.class, args);
 	}
-
 }
