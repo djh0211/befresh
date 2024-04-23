d0763244c9273e845934c723956d834a068f5d82
정승환 <seunghw2@naver.com>
2024-04-23 16:39:24 +0900

    [fix] BaseEntity 수정

--- backend/befresh/src/main/java/com/a307/befresh/module/domain/BaseEntity.java
+++ backend/befresh/src/main/java/com/a307/befresh/module/domain/BaseEntity.java
@@ -3,7 +3,9 @@ package com.a307.befresh.module.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -12,25 +14,38 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @MappedSuperclass
-@EntityListeners({AuditingEntityListener.class})
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    @Column(nullable = false)
+    private boolean delYn;
+
     @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createTime;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime regDttm;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Long regUserSeq;    // TODO : 자동 주입 추후 구현
 
     @LastModifiedDate
-    private LocalDateTime updateTime;
+    @Column(nullable = false)
+    private LocalDateTime modDttm;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private Long modUserSeq;    // TODO : 자동 주입 추후 구현
 
     @PrePersist
     public void prePersist() {
         LocalDateTime now = LocalDateTime.now();
-        this.createTime = now;
-        this.updateTime = now;
+        if (regDttm == null) { // only set if not already set
+            regDttm = now;
+        }
+        modDttm = now;
     }
 
     @PreUpdate
     public void preUpdate() {
-        LocalDateTime now = LocalDateTime.now();
-        this.updateTime = now;
+        modDttm = LocalDateTime.now();
     }
 }
