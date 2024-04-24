cf49ed10d0ca2a7d7f203ab4c085148f1673663c
chico <silverstuff@naver.com>
2024-04-24 15:28:39 +0900

    S10P31A307-47 [feat] 프론트 구조 구성 완료

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
+import IdInputTextFields from '../atoms/IdInput'; // inputbox.tsx 파일의 컴포넌트를 import
+import PasswordInputTextFields from '../atoms/PasswordInput';
+
 function LoginPage() {
   return (
     <div>
-      <h1>여기 로그인</h1>
+      {/* inputbox.tsx 파일에 정의된 컴포넌트 사용 */}
+      <IdInputTextFields />
+      <PasswordInputTextFields />
     </div>
   );
 }
