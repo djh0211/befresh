a91e4014f96f838b0815d6e3d890b2bff7ca3513
남수진 <a25062401@gmail.com>
2024-04-24 17:57:51 +0900

    [feat] 냉장고 Repository 추가

---
+++ backend/befresh/src/main/java/com/a307/befresh/module/domain/refrigerator/repository/RefrigeratorRepository.java
@@ -0,0 +1,7 @@
+package com.a307.befresh.module.domain.refrigerator.repository;
+
+import com.a307.befresh.module.domain.refrigerator.Refrigerator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefrigeratorRepository extends JpaRepository<Refrigerator, Long> {
+}
