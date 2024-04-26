b5083121ae6499a4347a9ffff393b98521ac9e45
정유경 <kadie@naver.com>
2024-04-26 13:56:21 +0900

    [feat] nginx 설정 수정

--- frontend/dev-dist/registerSW.js
+++ frontend/dev-dist/registerSW.js
@@ -1,1 +1,12 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+// if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+
+if ('serviceWorker' in navigator) {
+    let registrationOptions = {};
+
+    // "/jenkins" 경로에 대해서는 Service Worker를 등록하지 않습니다.
+    if (!window.location.pathname.startsWith('/jenkins')) {
+        registrationOptions = { scope: '/', type: 'classic' };
+    }
+
+    navigator.serviceWorker.register('/dev-sw.js?dev-sw', registrationOptions);
+}
