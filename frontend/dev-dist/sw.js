f433da672112ead7bb7bdf8e25fbbbca287aedc6
chico <silverstuff@naver.com>
2024-04-26 16:40:39 +0900

    [feat]mainpage

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.voui68e8heo"
+    "revision": "0.4n0mel6i138"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
