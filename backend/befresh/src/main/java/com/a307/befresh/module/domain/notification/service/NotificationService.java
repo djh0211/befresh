7805373c0cb0e4dbf593ff915284a0cde943c08f
정승환 <seunghw2@naver.com>
2024-05-02 13:09:15 +0900

    [feat] Spring Batch를 통한 유통기한 관련 알림 전송 기능 구현

--- backend/befresh/src/main/java/com/a307/befresh/module/domain/notification/service/NotificationService.java
+++ backend/befresh/src/main/java/com/a307/befresh/module/domain/notification/service/NotificationService.java
@@ -1,5 +1,11 @@
 package com.a307.befresh.module.domain.notification.service;
 
+import com.a307.befresh.module.domain.food.Food;
+
+import java.util.List;
+
 public interface NotificationService {
     void sendFreshWarnMessage();
+
+    void sendExpireNotification(long refrigeratorId, List<Food> foodList, int daysBefore);
 }
