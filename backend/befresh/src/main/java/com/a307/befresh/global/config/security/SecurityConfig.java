dc50c6bdc80826296657e2e46d891a8af2609df0
남수진 <a25062401@gmail.com>
2024-05-07 16:15:07 +0900

    [feat] csrf

--- backend/befresh/src/main/java/com/a307/befresh/global/config/security/SecurityConfig.java
+++ backend/befresh/src/main/java/com/a307/befresh/global/config/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//            .csrf(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
             .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
             .httpBasic(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
