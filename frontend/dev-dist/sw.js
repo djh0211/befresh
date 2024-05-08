49710fbbb59ee444f7b2b608b88f2a49b1e9b7fc
chico <silverstuff@naver.com>
2024-05-08 09:32:25 +0900

    [feat] 모달 작성중 임시공유

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,8 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    // "revision": "0.ahndka96aq" 
-    "revision": "0.ulibl2oj9i8"
+    "revision": "0.1bc0ee128mg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
