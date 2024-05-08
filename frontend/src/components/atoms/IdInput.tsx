067e613b4f6cd0dd75e1f719e91b5673e630852b
chico <silverstuff@naver.com>
2024-05-08 11:24:42 +0900

    [feat] css수정

--- frontend/src/components/atoms/IdInput.tsx
+++ frontend/src/components/atoms/IdInput.tsx
@@ -1,23 +1,28 @@
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 
-// TextField 컴포넌트를 상속하여 새로운 스타일을 적용하는 컴포넌트 생성
 const CustomTextField = styled(TextField)({
-  width: '200px', 
+  width:'300px',
+  height:'150px',
+  '& .MuiInputBase-input': {
+    fontSize: '16px',
+    width: '250px',
+    height: '50px',
+  },
   '& .MuiInputLabel-root': {
-    '&.Mui-focused': { 
-      color: 'green', 
+    '&.Mui-focused': {
+      color: 'green',
     },
   },
   '& .MuiInputBase-root': {
-    '&:after': { 
-      borderBottomColor: 'green', 
+    '&:after': {
+      borderBottomColor: 'green',
     },
   },
 });
 
 interface IdInputTextFieldsProps {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void; // 변경 이벤트 핸들러를 받을 props
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 function IdInputTextFields({ onChange }: Readonly<IdInputTextFieldsProps>) {
@@ -26,7 +31,7 @@ function IdInputTextFields({ onChange }: Readonly<IdInputTextFieldsProps>) {
       id="standard-basic"
       label="ID"
       variant="standard"
-      onChange={onChange} 
+      onChange={onChange}
     />
   );
 }
