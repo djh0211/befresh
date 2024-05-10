a2f1b2107551c34b55e5c0a211acf43d3fe819de
chico <silverstuff@naver.com>
2024-05-10 14:41:28 +0900

    [feat] 바로삭제

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h61eqkjsmf8"
+    "revision": "0.op83sk1lk6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/api/food/foodCardApi.ts
+++ frontend/src/api/food/foodCardApi.ts
@@ -17,3 +17,19 @@ export async function getFoods() {
     console.error('음식 실패:', error);
   }
 }
+
+export async function deleteFood(foodId: number) {
+  try {
+    const response = await axiosInstance.delete(`https://be-fresh.site/api/foods?foodId=${foodId}`);
+    if (response.status === 200) {
+      console.log('음식 삭제 성공');
+      // 삭제 성공 시 추가적인 작업을 수행할 수 있습니다.
+    } else {
+      console.error('음식 삭제 실패');
+      // 삭제 실패 시 에러 처리를 수행할 수 있습니다.
+    }
+  } catch (error) {
+    console.error('음식 삭제 중 오류 발생:', error);
+    // 오류 발생 시 에러 처리를 수행할 수 있습니다.
+  }
+}
