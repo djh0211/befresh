88673b5e349fca145a82b553b79b55841176e4bd
yeji <yeji1107@naver.com>
2024-05-10 18:02:21 +0900

    [feat] 용기 정보 페이지

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.92tdk8phse"
+    "revision": "0.9frvmj6fpho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ frontend/src/components/templates/infoTemp.tsx
@@ -0,0 +1,43 @@
+import NavBlock from "../molecules/navBlock";
+import styled from "styled-components";
+import InfoForm from "../organisms/InfoForm";
+
+const InfoMain = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const InfoTitle = styled.div`
+  text-align: center;
+  margin: 5vh 0 3vh 0;
+  font-size: 4rem;
+  font-weight: 900;
+`
+
+const NavBlockWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #ffffff; 
+  z-index: 999; 
+`;
+
+export default function InfoTemp() {
+  return (
+    <InfoMain>
+      <InfoTitle>용기 정보</InfoTitle>
+      <div>
+        <InfoForm/>
+        <InfoForm/>
+        <InfoForm/>
+        <InfoForm/>
+      </div>
+      
+      <NavBlockWrapper>
+        <NavBlock />
+      </NavBlockWrapper>
+    </InfoMain>
+  );
+}
