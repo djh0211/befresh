0bbb46bbb3769a4600290def9c3b289aca91ffe9
yeji <yeji1107@naver.com>
2024-05-14 09:11:21 +0900

    [feat] 알람 필터링 기능 추가

--- frontend/src/pages/AlarmPage.tsx
+++ frontend/src/pages/AlarmPage.tsx
@@ -2,7 +2,7 @@ import AlarmTemplate from "../components/templates/alarmTemp";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store/store";
 import { addAlarms, deleteAllAlarms, deleteOneAlarm, alertOff } from "../store/features/alarmSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAlarms, deleteAllAlarmsApi, deleteOneAlarmsApi } from "../api/alarm/alarmApi";
 import { getAccessToken } from "../utils/tokenUtils";
 
@@ -11,6 +11,8 @@ function AlarmPage() {
   const token = getAccessToken()
   const dispatch = useDispatch()
   const alarms = useSelector((state:RootState) => state.alarms)
+  const [alarmList, setAlarmList] = useState(alarms.alarms)
+
   // 전체 알람 삭제 api요청도 같이 보내주기
   const deleteAlarms = () => {
     if (token) {
@@ -27,24 +29,28 @@ function AlarmPage() {
   }
 
   // 알람 저장
-  const saveAlarm =async (token:string) => {
+  const saveAlarm = async (token:string) => {
     const apiAlarms = await getAlarms(token)
+    const filteredAlarm = apiAlarms.filter((alarm) => {
+      return alarms.categories.includes(alarm.data.category)
+    })
     dispatch(addAlarms(apiAlarms))
+    setAlarmList(filteredAlarm)
   }
 
   // 알람 페이지에 오면 알람 우선 꺼줌
   useEffect(() => {
     // 새로고침하면 있던거 삭제하고 api로 가져옴
     dispatch(deleteAllAlarms())
+    dispatch(alertOff())
     if (token) {
       saveAlarm(token)
     }
-    dispatch(alertOff())
   }, [])
 
   return (
     <div>
-      <AlarmTemplate alarms={alarms.alarms} deleteAlarms={deleteAlarms} deleteOne={deleteOne}/>
+      <AlarmTemplate alarms={alarmList} deleteAlarms={deleteAlarms} deleteOne={deleteOne}/>
     </div>
   );
 }
