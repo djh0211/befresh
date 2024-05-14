8eccd87b937a9e64de67ad3fe003380f1aa433d2
정승환 <seunghw2@naver.com>
2024-05-14 13:54:22 +0900

    [feat] Batch 관련 임시 API 구현

--- backend/befresh/src/main/java/com/a307/befresh/module/domain/food/controller/FoodController.java
+++ backend/befresh/src/main/java/com/a307/befresh/module/domain/food/controller/FoodController.java
@@ -1,6 +1,9 @@
 package com.a307.befresh.module.domain.food.controller;
 
 import com.a307.befresh.global.api.response.BaseResponse;
+import com.a307.befresh.global.config.batch.BatchScheduleConfig;
+import com.a307.befresh.global.config.batch.FoodExpireBatchConfig;
+import com.a307.befresh.global.config.batch.FoodSensorBatchConfig;
 import com.a307.befresh.global.exception.code.SuccessCode;
 import com.a307.befresh.global.security.domain.UserDetailsImpl;
 import com.a307.befresh.module.domain.food.dto.request.FoodRegisterReqList;
@@ -11,6 +14,7 @@ import com.a307.befresh.module.domain.food.dto.response.FoodListDetailRes;
 import com.a307.befresh.module.domain.food.service.FoodService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +29,10 @@ import java.util.List;
 public class FoodController {
 
     private final FoodService foodService;
+    private final BatchScheduleConfig batchScheduleConfig;
+    private final JobLauncher jobLauncher;
+    private final FoodSensorBatchConfig foodSensorBatchConfig;
+    private final FoodExpireBatchConfig foodExpireBatchConfig;
 
     @GetMapping("/test/thread")
     public ResponseEntity<ThreadInfo> getThreadInfo() throws InterruptedException {
@@ -88,5 +96,19 @@ public class FoodController {
         List<FoodFailRes> foodFailList = foodService.getFoodFailList(refrigeratorId);
 
         return BaseResponse.success(SuccessCode.SELECT_SUCCESS, foodFailList);
+    }
+
+    @GetMapping("/batch/expire")
+    public ResponseEntity<BaseResponse<Long>> processBatchExpire() {
+        batchScheduleConfig.runExpiredFoodJob();
+
+        return BaseResponse.success(SuccessCode.SELECT_SUCCESS, null);
+    }
+
+    @GetMapping("/batch/container")
+    public ResponseEntity<BaseResponse<Long>> processBatchContainer() {
+        batchScheduleConfig.runSensorJob();
+
+        return BaseResponse.success(SuccessCode.SELECT_SUCCESS, null);
     }
 }
