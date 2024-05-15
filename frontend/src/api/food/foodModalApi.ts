7b32b2f6f3c3b85cba95cfc4bb5a7e65c54fde83
chico <silverstuff@naver.com>
2024-05-15 17:03:28 +0900

    [feat] 빈화면 처리, 유통기한 처리

--- frontend/src/api/food/foodModalApi.ts
+++ frontend/src/api/food/foodModalApi.ts
@@ -1,4 +1,5 @@
 import axiosInstance from '../../utils/axiosConfig';
+import dayjs from 'dayjs';
 
 
 // 음식 상세 정보를 가져오는 함수
@@ -13,10 +14,19 @@ export async function modalFoodDetail(foodId: number): Promise<any> {
   }
 }
 
-export async function updateFoodDetail( updatedData: { foodId?: number, name?: string, expirationDate?: string }): Promise<void> {
+export async function updateFoodDetail(updatedData: {
+  foodId?: number;
+  name?: string;
+  expirationDate?: string;
+}): Promise<void> {
   try {
+    if (updatedData.expirationDate) {
+      const newExpirationDate = dayjs(updatedData.expirationDate).add(9, 'hour').toISOString();
+      updatedData.expirationDate = newExpirationDate;
+    }
+
     await axiosInstance.put(`https://be-fresh.site/api/foods`, updatedData);
-    console.log('수정됨?')
+    console.log('수정됨?');
   } catch (error) {
     console.log('음식 정보 수정 오류 :', error);
   }
